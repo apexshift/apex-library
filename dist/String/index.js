f1f08302e86e730312dbe85417fbb43560731a62
Apex Shift <webmaster@apexshift.co.uk>
2026-04-27 18:28:50 +0100

    chore: stop tracking dist/ folder (now properly gitignored)

--- dist/String/index.js
+++
@@ -1,6 +0,0 @@
-import { __exportAll as r } from "../_virtual/_rolldown/runtime.js";
-import { Effects_exports as t } from "./Effects/index.js";
-var f = /* @__PURE__ */ r({ Effects: () => t });
-export {
-  f as String_exports
-};
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { String_exports as r } from "./String/index.js";
-console.log("Apex library loaded.");
-export {
-  r as String
-};
