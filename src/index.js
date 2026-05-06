409895a8c83c586cc29b23d60e0be57cfe07c288
Apex Shift <webmaster@apexshift.co.uk>
2026-05-06 18:33:24 +0100

    chore: added missing packages and re-ordered

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * as Core from './Core/index.js';
+export * as DX from './DX/index.js';
 export * as Event from './Event/index.js';
 export * as Maths from './Maths/index.js';
-export * as String from './String/index.js';
 export * as Performance from './Performance/index.js';
+export * as String from './String/index.js';
